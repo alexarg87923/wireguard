a612f7b9109f19705d79e2aa48f81ffbd9d20736
Alex Arguelles <alexarguelles87923@gmail.com>
2025-09-09 17:07:14 -0400

    exposing ports

--- start_container.sh
+++ start_container.sh
@@ -5,6 +5,7 @@ if ! docker start wireguard 2>/dev/null; then
         --cap-add=NET_ADMIN \
         --name=wireguard \
         -d \
+        -p 51821:51821/udp \
         -v ./keys:/config/server \
         -v ./wireguard:/config/wg_confs \
         wireguard \
