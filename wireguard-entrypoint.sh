efcf6308baf47b5b84cd04ddcfc3e182d85da8fa
Alex Arguelles <alexarguelles87923@gmail.com>
2025-09-14 22:39:43 -0400

    fixing init script

--- wireguard-entrypoint.sh
+++ wireguard-entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
 PEER_PSK="/config/wg_confs/peer1.psk"
+CONFIG_DIR="./config/wg_confs"
+KEY_DIR="./keys"
+
+if [ ! -f "$KEY_DIR/privatekey-client" ]; then
+    echo "No client keypair found, generating..."
+    umask 077
+    wg genkey | tee "$KEY_DIR/privatekey-client" | wg pubkey > "$KEY_DIR/publickey-client"
+fi
+
+cat > "$CONFIG_DIR/wg0.conf" <<EOF
+[Interface]
+Address = $(cat ${CONFIG_DIR}/client_ip)
+PrivateKey = $(cat ${KEY_DIR}/privatekey-client)
+$( [ -f "${CONFIG_DIR}/dns" ] && echo "DNS = $(cat ${CONFIG_DIR}/dns)" )
+
+[Peer]
+PublicKey = $(cat ${CONFIG_DIR}/peer1.pub)
+$( [ -f "${CONFIG_DIR}/peer1.psk" ] && echo "PresharedKey = $(cat ${CONFIG_DIR}/peer1.psk)" )
+Endpoint = $(cat ${CONFIG_DIR}/endpoint):$(cat ${CONFIG_DIR}/port)
+AllowedIPs = $(cat ${CONFIG_DIR}/allowed_ips)
+EOF
+echo "Generated client wg0.conf"
 
 if [ ! -f "$PEER_PSK" ]; then
     wg genpsk > "$PEER_PSK"
