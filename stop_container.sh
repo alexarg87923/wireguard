61e158675ce073373d118998f739bf49c2adf882
Alex Arguelles <alexarguelles87923@gmail.com>
2025-09-17 20:33:59 -0400

    cleaning up scripts

--- stop_container.sh
+++ stop_container.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
 CONTAINER_NAME=wireguard
-
 CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $CONTAINER_NAME)
+ENDPOINT_FILE=./config/endpoint
 
-if [ -n "$CONTAINER_IP" ]; then
-    echo "Removing NAT rules for $CONTAINER_IP"
-    sudo iptables -t nat -D POSTROUTING -s $CONTAINER_IP -j MASQUERADE 2>/dev/null
-    sudo iptables -t nat -D PREROUTING -j DNAT --to-destination $CONTAINER_IP 2>/dev/null
+if [ -f "$ENDPOINT_FILE" ]; then
+  if [ -n "$CONTAINER_IP" ]; then
+      echo "Removing NAT rules for $CONTAINER_IP"
+      sudo iptables -t nat -D POSTROUTING -s $CONTAINER_IP -j MASQUERADE 2>/dev/null
+      sudo iptables -t nat -D PREROUTING -j DNAT --to-destination $CONTAINER_IP 2>/dev/null
+  fi
 fi
 
 docker stop $CONTAINER_NAME 2>/dev/null
